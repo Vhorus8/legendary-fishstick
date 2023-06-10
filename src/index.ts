16c9d75e793b7a1b10103377d46fbf9e31bf78f3
Vhorus8 <135922681+Vhorus8@users.noreply.github.com>
2023-06-10 17:45:21 -0300

    Shin hat
    Puse a shin chan y un sombrero.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Container, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,60 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
 
-clampy.anchor.set(0.5);
+// adaptar tamaño lienzo a ventana
+window.addEventListener("resize", ()=>{
+	
+	const scaleX = window.innerWidth / app.screen.width; //ancho navegador dividido x ancho lienzo
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX,scaleY);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+	
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+	
+	// const view = app.view as HTMLCanvasElement;
 
-app.stage.addChild(clampy);
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px"; //marginHorizontal.toString()  para convertir a string
+	// app.view.style.marginRight = marginHorizontal + "px";
+	app.view.style.marginTop = marginVertical + "px";
+	// app.view.style.marginBottom = marginVertical + "px";
+});
+window.dispatchEvent(new Event("resize"));
+
+
+Loader.shared.add({url: "./clampy.png", name: "Clampy"});
+Loader.shared.add({url: "./shin-chan.png", name: "Shinchan"});
+Loader.shared.add({url: "./hat.png", name: "Hat"});
+
+Loader.shared.onComplete.add(()=>{  //al completar la carga d lo anterior (loader), ejecutar la siguiente función
+const shin: Sprite = Sprite.from("Shinchan");
+const hat: Sprite = Sprite.from("Hat");
+
+shin.anchor.set(0.5);
+hat.anchor.set(0.5);
+hat.scale.set(0.2);
+hat.position.set(70,-210);
+
+const shinWithHat: Container = new Container();
+
+shinWithHat.addChild(shin);  // agregar una instancia al stage
+shinWithHat.addChild(hat);
+
+shinWithHat.scale.set(0.5);
+shinWithHat.position.set(320,240);
+
+app.stage.addChild(shinWithHat);
+
+console.log(hat.parent.toGlobal(hat.position));
+});
+
+Loader.shared.load(); // ?
+
+
+
